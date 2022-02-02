381fbccffa27333a1c0d91acd6717bb6be5d04fe
BrentMeeusen <brentm@live.nl>
2022-02-02 21:55:49 +0100

    Added Javadoc

--- src/main/java/com/minifiedspotifywrapped/Application.java
+++ src/main/java/com/minifiedspotifywrapped/Application.java
@@ -2,6 +2,16 @@ package com.minifiedspotifywrapped;
 
 public class Application {
 
+    /**
+     * The method that will run.
+     *
+     * First argument (required): path to downloaded Spotify data.
+     * Optional arguments:
+     *   -f     Full output
+     *   -n [n] Number of tracks and artists to show. Default: 10
+     *
+     * @param args The arguments
+     */
     public static void main(String[] args) {
 
     }
