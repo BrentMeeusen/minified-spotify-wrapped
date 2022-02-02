858c4c29e1ddb60f4125b0cc9b00f5e7ee5b5d7a
BrentMeeusen <brentm@live.nl>
2022-02-02 23:38:29 +0100

    Added getStreamingHistoryFiles(String path) method

--- src/main/java/com/minifiedspotifywrapped/Application.java
+++ src/main/java/com/minifiedspotifywrapped/Application.java
@@ -1,5 +1,8 @@
 package com.minifiedspotifywrapped;
 
+import java.io.File;
+import java.util.ArrayList;
+
 public class Application {
 
     /**
@@ -16,10 +19,53 @@ public class Application {
 
         // If path is not filled in: return
         if(args.length == 0) {
-            System.out.println("Please provide the path to the Spotify Data folder." +
+            System.out.println("Please provide the path to the Spotify Data folder. " +
                 "You can request the data here: https://www.spotify.com/account/privacy/");
             return;
         }
+
+        // Get the Streaming History files
+        ArrayList<File> history = getStreamingHistoryFiles(args[0]);
+        if(history == null) {
+            return;
+        }
+
+        System.out.println(history);
+
+    }
+
+
+    /**
+     * Gets the files
+     *
+     * @param path
+     * @return
+     */
+    private static ArrayList<File> getStreamingHistoryFiles(String path) {
+
+        // Initialise ArrayList, get path to directory
+        ArrayList<File> history = new ArrayList<>();
+        File directory = new File(path);
+
+        // If it's not a directory, return false
+        if(!directory.isDirectory()) {
+            System.out.println("The given path is not a directory.");
+            return null;
+        }
+
+        // Get history files
+        int i = 0;
+        while(true) {
+            File file = new File(directory.getAbsolutePath() + "\\StreamingHistory" + i++ + ".json");
+            if(file.isFile()) {
+                history.add(file);
+            } else {
+                break;
+            }
+        }
+
+        // Return files found
+        return history;
 
     }
 
