ad4010f38a0a3a2ad51b46277b34569115586b5e
BrentMeeusen <brentm@live.nl>
2022-02-03 01:26:17 +0100

    Added constructor

--- src/main/java/com/minifiedspotifywrapped/Stream.java
+++ src/main/java/com/minifiedspotifywrapped/Stream.java
@@ -2,15 +2,14 @@ package com.minifiedspotifywrapped;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Date;
+import java.util.Calendar;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class Stream {
 
-    private Date endTime;
+    private Calendar endTime;
     private String artist;
     private String track;
     private int msPlayed;
@@ -21,6 +20,23 @@ public class Stream {
      * @param stream The format
      */
     private Stream(String stream) {
+
+        // Create a scanner
+        Scanner scanner = null;
+        scanner = new Scanner(stream);
+        scanner.useDelimiter(Pattern.compile("\"?,?\\s*\"[\\w\\W][^\"]*\"\\s*:\\s*\"?"));
+
+        // Get endTime
+        String[] values = scanner.next().split("[-\\s:]");
+        endTime = Calendar.getInstance();
+        endTime.set(Integer.parseInt(values[0]), Integer.parseInt(values[1]) - 1,
+            Integer.parseInt(values[2]), Integer.parseInt(values[3]), Integer.parseInt(values[4])
+        );
+
+        // Get artist, track, msPlayed
+        artist = scanner.next();
+        track = scanner.next();
+        msPlayed = scanner.nextInt();
 
     }
 
@@ -53,6 +69,7 @@ public class Stream {
             // Read files
             while(scanner.hasNext()) {
                 streams.add(new Stream(scanner.next()));
+                break;
             }
 
         } // for(File file : Files)
