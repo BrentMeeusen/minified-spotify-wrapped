6622d9e61160cfdbd85597421f08b94e50791b31
BrentMeeusen <brentm@live.nl>
2022-02-18 22:50:16 +0100

    Removed old code, setup skeleton for new code

--- src/main/java/com/minifiedspotifywrapped/Application.java
+++ src/main/java/com/minifiedspotifywrapped/Application.java
@@ -2,90 +2,65 @@ package com.minifiedspotifywrapped;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class Application {
 
-	private static void print(String[] arr) {
-		for(String s : arr) {
-			System.out.print(s + ", ");
-		}
-		System.out.println("done.");
-	}
-
-    /**
-     * The method that is called when the program is opened.
-     *
-     * @param args The arguments
-     */
-    public static void main(String[] args) {
+	/**
+	 * The method that is called when the program is opened.
+	 *
+	 * @param args The arguments
+	 */
+	public static void main(String[] args) {
 
 		// Initialise a scanner that takes user input and print the opening
-	    System.out.println("Minified Spotify Wrapped\r\n" +
-		                   "========================\r\n");
+		System.out.println("Minified Spotify Wrapped\r\n" +
+			"========================\r\n");
 		showCommands();
 
-	    Scanner user = new Scanner(System.in);
+		Scanner user = new Scanner(System.in);
 
-	    // Ask for command and check its validity
-	    String command = user.next().toLowerCase(Locale.ROOT);
-	    while(!(command.equals("exit") || command.equals("quit"))) {
+		// Ask for command and check its validity
+		String command = user.next().toLowerCase(Locale.ROOT);
+		while (!(command.equals("exit") || command.equals("quit"))) {
 
 			// Select command
-		    switch(command) {
+			switch (command) {
 
-			    case "help":
+				case "help":
 					showCommands();
 					break;
 
-			    case "":
+				case "variables":
+					showVariables();
 					break;
 
-			    default:
-					System.out.println("Unknown command. Please type \"help\" to see a list of commands.");
+				case "path":
+				case "amount":
+				case "full":
+				case "year":
+					Stream.setVariable(command);
 
-		    }
+				case "show":
+					Stream.showResults();
+
+				case "save":
+					Stream.saveResults();
+
+				default:
+					System.out.println(
+						"Unknown command. Please type \"help\" to see a list of commands.");
+
+			}
 
 			// Ask for next command
 			command = user.next();
 
-	    }
+		}
 
-
-
-
-//        // If path is not filled in: return
-//        if(args.length == 0) {
-//            System.err.println("Please provide the path to the Spotify Data folder. " +
-//                "You can request the data here: https://www.spotify.com/account/privacy/");
-//            return;
-//        }
-//
-//        // Get the Streaming History files
-//        ArrayList<File> history = getStreamingHistoryFiles(args[0]);
-//        if(history == null) {
-//            return;
-//        } else if(history.size() == 0) {
-//            System.err.println("There are no files in the directory that match the format. " +
-//                "Please make sure that the directory is correct.");
-//			return;
-//        }
-//
-//		// Get the flags
-//	    int amount = getAmountFromFlags(args);
-//	    if(amount == -99) { return; }
-//
-//	    int year = getYearFromFlags(args);
-//	    if(year == -99) { return; }
-//
-//        // Parse JSON files to Stream instances and generate report
-//        Stream.generate(history);
-//        System.out.println(Stream.generateReport(amount, year));
-
-    }
+	}
 
 
 	/**
@@ -98,91 +73,18 @@ public class Application {
 			"path - Specify the path to the folder in which the StreamingHistoryX.json files are located.\r\n" +
 			"amount - Set the number of tracks and artists to show.\r\n" +
 			"full - Show the full report, including all tracks and artists.\r\n" +
-			"year - Set the year for which the data should be looked up.\r\n");
-	}
-
-
-    /**
-     * Gets the Streaming History files.
-     *
-     * @param path The directory.
-     * @return an ArrayList of files
-     */
-    private static ArrayList<File> getStreamingHistoryFiles(String path) {
-
-        // Initialise ArrayList, get path to directory
-        ArrayList<File> history = new ArrayList<>();
-        File directory = new File(path);
-
-        // If it's not a directory, return false
-        if(!directory.isDirectory()) {
-            System.err.println("The given path is not a directory.");
-            return null;
-        }
-
-        // Get history files
-        int i = 0;
-		File file = new File(directory.getAbsolutePath() + "\\StreamingHistory" + i++ + ".json");
-        while(file.isFile()) {
-			history.add(file);
-            file = new File(directory.getAbsolutePath() + "\\StreamingHistory" + i++ + ".json");
-        }
-
-        // Return files found
-        return history;
-
-    }
-
-
-	/**
-	 * Gets the amount of artists and tracks to list from the flags.
-	 *
-	 * @param flags The flags to search in
-	 * @return The amount
-	 */
-	private static int getAmountFromFlags(String[] flags) {
-
-		int amount = 10;
-		for(int i = 0; i < flags.length; i++) {
-			if(flags[i].equals("-f")) {
-				amount = -1;
-			} else if(flags[i].equals("-n")) {
-				try {
-					amount = Integer.parseInt(flags[++i]);
-				}
-				catch(Exception e) {
-					System.err.println("Please insert an integer as limit.");
-					return -99;
-				}
-			}
-		}
-		return amount;
-
+			"year - Set the year for which the data should be looked up.\r\n" +
+			"show - Shows the output in the console.\r\n" +
+			"save - Saves the output in the supported formats you wish. The output is saved in the folder where it is reading the data from.\r\n" +
+			"exit/quit - Quit the program.\r\n");
 	}
 
 
 	/**
-	 * Gets the amount of artists and tracks to list from the flags.
-	 *
-	 * @param flags The flags to search in
-	 * @return The amount
+	 * Prints the variables with which the program will calculate the Wrapped statistics.
 	 */
-	private static int getYearFromFlags(String[] flags) {
-
-		int year = Calendar.getInstance().get(Calendar.YEAR);
-		for(int i = 0; i < flags.length; i++) {
-			if(flags[i].equals("-y")) {
-				try {
-					year = Integer.parseInt(flags[++i]);
-				}
-				catch(Exception e) {
-					System.err.println("Please insert an integer as year.");
-					return -99;
-				}
-			}
-		}
-		return year;
-
+	private static void showVariables() {
+		System.out.println(Stream.getVariables());
 	}
 
 }
